7cf6719698951a742d3606aa4df0ac0038d214a1
juny225 <juny225@naver.com>
2023-11-20 14:58:35 +0900

    first commit

---
+++ src/day01/EX01.java
@@ -0,0 +1,32 @@
+package day01;
+
+class Ex01 {
+    public static void main(String[] args) {
+        Integer num1;
+        num1=10;
+
+        Integer num2;
+        num2=20;
+
+        // 다음에 코드를 작성하여 num1과 num2에 저장된 숫자를 서로 바꾸시오
+        // 단 num1 = 20;, num2=10; 사용금지
+        System.out.println(num1);
+        System.out.println(num2);
+
+        if (num1 > num2) {
+            Integer num3 = num1;
+            num1 = num2;
+            num2 = num3;
+        } else {
+            Integer num3 = num2;
+            num2 = num1;
+            num1 = num3;
+        }
+
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
+
+
+
